30beda22ca1744c0fe4e708f064fd5e4da983c17
GettingStarted.app <shayaan.siddiqui@gmail.com>
2023-12-28 09:22:36 -0500

    Prevent crash if user answered all questions and present a Trophy

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,29 +1,38 @@
 import {useState} from "react";
 import QUESTIONS from '../assets/questions.js'
+import quizCompleted from '../assets/quiz-complete.png'
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
-
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
-
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswers((prevState) => {
             return [...prevState, selectedAnswer]
         });
     }
 
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+    if(quizIsComplete) {
+        return (<div id={'summary'}>
+            <img src={quizCompleted} alt={'Quiz Completed Trophy'} />
+            <h2>Quiz Completed</h2>
+        </div>)
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id={'quiz'}>
             <div id={'question'}>
                 <p>{QUESTIONS[activeQuestionIndex].text}</p>
                 <ul id={'answers'}>
-                    {shuffledAnswers.map((item, index) => (
-                        <li key={index} className={'answer'}>
-                            <button onClick={() => handleSelectAnswer(item)}>{item}</button>
-                        </li>
-                    ))
+                    {
+                        shuffledAnswers.map((item, index) => (
+                            <li key={index} className={'answer'}>
+                                <button onClick={() => handleSelectAnswer(item)}>{item}</button>
+                            </li>
+                        ))
                     }
                 </ul>
             </div>
